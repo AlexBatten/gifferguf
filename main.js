71faa858544c6fb075176ecb1e506792d033c720
Alex Batten <alex.batten@live.com>
2023-02-06 16:06:18 +0100

    giffer guf

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Hello Giffer!</h1>
+    <img src="https://media3.giphy.com/media/1ET7hRlCcZLuE/giphy.gif" alt="jif vs gif">
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
